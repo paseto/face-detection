9fb53d65a32ad289c612a08cdcb8529e0a12203f
giovaniw2@gmail.com <giovaniw2@gmail.com>
2019-06-26 10:52:23 -0300

    Rotate image

--- detect.py
+++ detect.py
@@ -79,8 +79,12 @@ def fix_orientation(img, save_over=False):
 def detect(path):
     """Detect if picture has a face, returns false or emotion detection prediction (happy, sad, angry, etc)"""
 
+    pathimage = Image.open(path)
+
     # Rotate image
-    fix_orientation(path, path)
+    messed = base_dir + "/outs/rotate.jpeg"
+    pathimage.save(messed)
+    fix_orientation(messed)
 
     image = face_recognition.load_image_file(path)
     face_locations = face_recognition.face_locations(image)
